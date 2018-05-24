d0f4327c7f4ef757e05ae44f44ad1e8c8a642be8
johnkouk469 <37601899+johnkouk469@users.noreply.github.com>
2018-05-24 14:04:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+unsigned int fact(int n)
+{
+  if (n < 1)
+    return 1;
+  else
+    return n*fact(n-1);
+
 int main()
 {
-  cout << "Test" << endl;
+  unsigned int i;
+  cin >> i;
+  cout << "Factorial: " << fact(i) << endl;
   return 0;
 }
